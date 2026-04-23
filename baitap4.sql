a3d661650fe4e82a7deec16bbaa6cc21cb590619
0309quochung-oss <0309quochung@gmail.com>
2026-04-23 23:02:59 +0700

    Update baitap4.sql

--- baitap4.sql
+++ baitap4.sql
@@ -1,3 +1,17 @@
-UPDATE PRODUCTS
-SET OriginalPrice = OriginalPrice * 0.9
-WHERE Category = 'Electronics';
+-- Soft Delete: Đánh dấu các đơn hàng bị hủy
+UPDATE ORDERS
+SET IsDeleted = 1
+WHERE Status = 'Canceled';
+
+-- Truy vấn cho hệ thống bán hàng 
+SELECT * 
+FROM ORDERS 
+WHERE IsDeleted = 0;
+
+-- Truy vấn cho kế toán 
+SELECT * 
+FROM ORDERS 
+WHERE Status = 'Canceled';
+
+-- Em chọn Soft Delete vì không làm mất dữ liệu, vẫn phục vụ kiểm toán.
+-- Hard Delete sẽ xóa vĩnh viễn nên không phù hợp.
